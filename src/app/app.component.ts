fc4f78234c7447d26c8e15b2bd33d54f2a07d65f
Federico Brinkworth <federico.brinkworth@gmail.com>
2017-04-09 23:57:29 -0300

    Add firebase basic auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,10 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { AngularFire } from 'angularfire2';
 
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
 
 @Component({
   templateUrl: 'app.html'
@@ -13,19 +14,23 @@ import { Page2 } from '../pages/page2/page2';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = Page1;
+  rootPage: any;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{ title: string, component: any }>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, public af: AngularFire) {
+
+    const authObserver = af.auth.subscribe(user => {
+      if (user) {
+        this.rootPage = HomePage;
+        authObserver.unsubscribe();
+      } else {
+        this.rootPage = LoginPage;
+        authObserver.unsubscribe();
+      }
+    });
+
     this.initializeApp();
-
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
-    ];
-
   }
 
   initializeApp() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,62 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+// Importing Pages
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { ResetPasswordPage } from '../pages/reset-password/reset-password';
+import { SignupPage } from '../pages/signup/signup';
+
+// Importing Providers
+import { AuthData } from '../providers/auth-data';
+
 // Import the AF2 Module
-import { AngularFireModule } from 'angularfire2';
- 
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+
 // AF2 Settings
 export const firebaseConfig = {
   apiKey: "AIzaSyCTmgffJOarQaVBErI1nrpY6B5AktVQ_sY",
   authDomain: "keiro-4e3c5.firebaseapp.com",
   databaseURL: "https://keiro-4e3c5.firebaseio.com",
-  projectId: "keiro-4e3c5",  
+  projectId: "keiro-4e3c5",
   storageBucket: "keiro-4e3c5.appspot.com",
   messagingSenderId: "77791368924"
+};
+
+export const myFirebaseAuthConfig = {
+  provider: AuthProviders.Password,
+  method: AuthMethods.Password
 };
 
 @NgModule({
   declarations: [
     MyApp,
-    Page1,
-    Page2
+    HomePage,
+    LoginPage,
+    ResetPasswordPage,
+    SignupPage
   ],
   imports: [
     IonicModule.forRoot(MyApp),
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(firebaseConfig, myFirebaseAuthConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    Page1,
-    Page2
+    HomePage,
+    LoginPage,
+    ResetPasswordPage,
+    SignupPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    AuthData,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
